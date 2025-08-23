2bb6ffb87b51966ee37fd5e4e3bf571371fb6593
DaniGomAris <danielgomezaristizabal2020@gmail.com>
2025-08-22 23:23:29 -0500

    Initial commit - Flask backend

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+from .extensions import jwt, limiter
+from .services.firebase import db
+from .routes.user_route import user_bp
+from config import config_by_name
+import os
+
+def create_app():
+    app = Flask(__name__)
+    
+    env = os.getenv("FLASK_ENV", "development")
+    app.config.from_object(config_by_name[env])
+
+    CORS(app)
+    jwt.init_app(app)
+    limiter.init_app(app)
+
+    app.register_blueprint(user_bp, url_prefix="/users")
+
+    return app
---
+++ app/models/user.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, document: str, document_type: str, name: str, last_name1: str, last_name2: str, email: str, password: str, phone: int):
+        self.document = document 
+        self.document_type = document_type
+        self.name = name
+        self.last_name1 = last_name1
+        self.last_name2 = last_name2
+        self.email = email
+        self.password = password
+        self.phone = phone
+        
+    
+    def to_dict(self):
+        """
+        Converts the User object into a dictionary to be stored in Firestore
+        Excludes fields like password confirmation and document ID (used as document name)
+        """
+        return {
+            'document_type': self.document_type,
+            'name': self.name,
+            'last_name1': self.last_name1,
+            'last_name2': self.last_name2,
+            'email': self.email,
+            'password': self.password,
+            'phone': self.phone
+        }
